f743c53a4663b18c63ad06dcf453b1ad27233c0a
abdull461 <abdullahabubakar461@gmail.com>
2023-07-30 23:08:20 +0100

    hidden secrets

--- app.py
+++ app.py
@@ -1,12 +1,33 @@
 from website import create_app
 import mysql.connector
+from dotenv import load_dotenv
+import os 
 
+load_dotenv()
+
+config = {
+    'user': os.getenv('USERNAME'),
+    'password': os.getenv('PASSWORD'),
+    'host': os.getenv('HOST'),
+    'database': os.getenv('DATABASE'),
+    'ssl_verify_identity': True,
+    'ssl_ca': '/etc/ssl/cert.pem',
+}
+
+cnx = mysql.connector.connect(**config)
+cnx.close()
+
+print("Successfully connected to PlanetScale!")
+
+''''
 mydb = mysql.connector.connect(
     host = "localhost",
     user = "root",
     passwd = "lludbA090",
 )
 my_cursor = mydb.cursor()
+'''
+
 
 app = create_app()
 
--- db.py
+++ db.py
@@ -1,12 +1,46 @@
 import mysql.connector
+import MySQLdb
+
+'''
+
+ Note: If you're developing on Windows, we recommend using mysql-connector-python instead
+  of mysqlclient as it is easier to enable SSL.
 
 db = mysql.connector.connect(
-    host = "gcp.connect.psdb.cloud",
-    user = "pgdaxrj9kn5ooadbodu0" ,
-    passwd = "pscale_pw_MhtAbC6zEJCYsfkujE4IPaw1CGpsC05vGD74bwGHgzt",
+    host = "",
+    user = "" ,
+    passwd = "",
 )
 
 my_cursor = db.cursor()
-my_cursor.execute("SHOW DATABASES")
-for db in my_cursor:
-    print(db)
+
+
+db = MySQLdb.connect(
+    host     = "[HOSTNAME]",
+    user     = "[USERNAME]",
+    passwd   = "[PASSWORD]",
+    db       = "[DATABASE]",
+    ssl_mode = "VERIFY_IDENTITY",
+    ssl      = {
+        "ca": "/etc/ssl/certs/ca-certificates.crt"
+    })
+
+
+
+import mysql.connector
+
+config = {
+    'user': '[USERNAME]',
+    'password': '[PASSWORD]',
+    'host': '[HOSTNAME]',
+    'database': '[DATABASE]',
+    'ssl_verify_identity': True,
+    'ssl_ca': '/etc/ssl/cert.pem',
+}
+
+cnx = mysql.connector.connect(**config)
+cnx.close()
+
+print("Successfully connected to PlanetScale!")
+
+'''
