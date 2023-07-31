29cae11448e957c42754a672ba9a94c81378b7e3
abdull461 <abdullahabubakar461@gmail.com>
2023-07-31 10:26:31 +0100

    connected to planetscale remote database

--- app.py
+++ app.py
@@ -2,32 +2,41 @@ from website import create_app
 import mysql.connector
 from dotenv import load_dotenv
 import os 
-
+import MySQLdb
+from sqlalchemy import create_engine, text
 load_dotenv()
 
-config = {
-    'user': os.getenv('USERNAME'),
-    'password': os.getenv('PASSWORD'),
-    'host': os.getenv('HOST'),
-    'database': os.getenv('DATABASE'),
-    'ssl_verify_identity': True,
-    'ssl_ca': '/etc/ssl/cert.pem',
-}
 
-cnx = mysql.connector.connect(**config)
-cnx.close()
+''''
+cnx = MySQLdb.connect(
+    user = os.getenv('USERNAME'),
+    password = os.getenv('PASSWORD'),
+    host = os.getenv('HOST'),
+    database = os.getenv('DATABASE'),
+    autocommit = True,
+    ssl_mode = "VERIFY_IDENTITY",
+    ssl      = {
+    'ca': "/etc/ssl/cert.pem",
+    'capath': "/path/to/certs",
+  }
+)
+   
 
 print("Successfully connected to PlanetScale!")
 
-''''
-mydb = mysql.connector.connect(
-    host = "localhost",
-    user = "root",
-    passwd = "lludbA090",
-)
-my_cursor = mydb.cursor()
-'''
 
+connection = mysql.connector.connect(
+  host= os.getenv("HOST"),
+  user=os.getenv("USERNAME"),
+  passwd= os.getenv("PASSWORD"),
+  db= os.getenv("DATABASE"),
+  autocommit = True,
+  ssl_mode = "VERIFY_IDENTITY",
+)
+my_cursor = connection.cursor()
+'''
+connect = os.getenv('SQLALCHEMY_DATABASE_URI')
+engine = create_engine(connect)
 
 app = create_app()
 
